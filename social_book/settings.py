e2280ad53970a1544ff718677aeb2badaac29b65
gohil-1234 <pruthvi33003@gmail.com>
2022-08-02 20:43:25 +0530

    added heroku url

--- social_book/settings.py
+++ social_book/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-f_pww4)6+-9mncvrufp#r*!cv^d3v@n-sa%3w97^wc$jdie1nt
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1', '.herokuapp.com']
+ALLOWED_HOSTS = ['127.0.0.1', 'get-together-1819.herokuapp.com']
 
 
 # Application definition
